27d9079925749fed5224bbde1f992cd60efafe5a
shmd001 <cool.shoronov19@gmail.com>
2023-04-03 18:25:39 +0300

    Fixed `NUMBER` to `INTEGER`

--- server/models/models.js
+++ server/models/models.js
@@ -3,10 +3,10 @@ const {DataTypes} = require('sequelize')
 
 const Company = sequelize.define("company", {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    inn: {type: DataTypes.NUMBER, unique: true},
+    inn: {type: DataTypes.INTEGER, unique: true},
     name: {type: DataTypes.STRING},
-    ogrn: {type: DataTypes.NUMBER, unique: true},
-    kpp: {type: DataTypes.NUMBER},
+    ogrn: {type: DataTypes.INTEGER, unique: true},
+    kpp: {type: DataTypes.INTEGER},
 })
 
 module.exports = {Company}
