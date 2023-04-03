efba4d3ca1db5c85d56386afaeba2478f078b811
shmd001 <cool.shoronov19@gmail.com>
2023-04-03 18:04:54 +0300

    Added middlewares to server app

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./database')
+const models = require('./models/models')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
 
 
 const start = async () => {
