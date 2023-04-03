9b1d72b8396b8bd0536c258e43da2db185b04064
shmd001 <cool.shoronov19@gmail.com>
2023-04-03 20:26:29 +0300

    Fixed model fields from `INTEGER` to `STRING`

--- server/models/models.js
+++ server/models/models.js
@@ -3,10 +3,10 @@ const {DataTypes} = require('sequelize')
 
 const Company = sequelize.define("company", {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    inn: {type: DataTypes.INTEGER, unique: true},
+    inn: {type: DataTypes.STRING, unique: true},
     name: {type: DataTypes.STRING},
-    ogrn: {type: DataTypes.INTEGER, unique: true},
-    kpp: {type: DataTypes.INTEGER},
+    ogrn: {type: DataTypes.STRING, unique: true},
+    kpp: {type: DataTypes.STRING},
 })
 
 module.exports = {Company}
