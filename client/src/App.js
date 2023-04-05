c063c4ca53828789d56db2fc3603dc19b551950f
shmd001 <cool.shoronov19@gmail.com>
2023-04-05 22:46:07 +0300

    Developed React app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,77 @@
-import React from "react";
+import React, {useState} from "react";
+import Grid from '@mui/material/Unstable_Grid2';
+import {
+    Button,
+    Container, Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableRow,
+    TextField
+} from "@mui/material";
+import axios from "axios";
+
 
 function App() {
+
+    const [company, setCompany] = useState({})
+    const [inn, setInn] = useState("")
+
+    const search = async () => {
+        const apiUrl = 'http://localhost:5000/api/company/?inn=' + inn
+
+        axios.get(apiUrl)
+            .then((resp) => {
+                const fetchedCompany = resp.data;
+                setCompany(fetchedCompany);
+            })
+            .catch((error) => {
+                setCompany({"Ошибка": "Нет информации о компании"})
+                console.log(error)
+            })
+    }
+
+    const handleInn = (e) => {
+        setInn(e.target.value)
+    }
+
     return (
         <div className="App">
-            Hello!
+            <Container maxWidth="sm">
+                <Grid container spacing={2}>
+                    <Grid xs={8}>
+                        <TextField onChange={handleInn} fullWidth={true} label={'ИНН'}/>
+                    </Grid>
+                    <Grid xs={4}>
+                        <Button
+                            fullWidth={true}
+                            style={{"height": "100%"}}
+                            variant="contained"
+                            color="success"
+                            onClick={search}
+                        >
+                            Найти
+                        </Button>
+                    </Grid>
+                    <Grid xs={12}>
+                        <TableContainer component={Paper}>
+                            <Table>
+                                <TableBody>
+                                    {Object.keys(company).map((key, index) => (
+                                        <TableRow
+                                            key={key}
+                                        >
+                                            <TableCell component="th" scope="row">{key}</TableCell>
+                                            <TableCell>{company[key]}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Grid>
+                </Grid>
+            </Container>
         </div>
     );
 }
